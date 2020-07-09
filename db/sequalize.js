900686bdc507ed4a625a65895a20207ef6864a32
ibrahim <imh19970408@gmail.com>
2020-07-09 13:51:14 +0300

    asd

--- db/sequalize.js
+++ db/sequalize.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
-  "postgres://postgres_admine:Eng1061995@sec-test-db.cugpacmdjttt.us-east-1.rds.amazonaws.com:5432/sec_db"
+  "postgres://postgres_admin:Eng1061995@sec-test-db.cugpacmdjttt.us-east-1.rds.amazonaws.com:5432/sec_db"
 );
 sequelize
   .authenticate()
